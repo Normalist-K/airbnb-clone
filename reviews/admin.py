f4993c86e7ad787035c7019a71b880083575de31
Normalist <uvento06@hanmail.net>
2019-11-14 23:46:39 +0100

    #9.0

---
+++ reviews/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.review)
+class ReviewAdmin(admin.ModelAdmin):
+
+    """ Review Admin Definition """
+
+    list_display = ("__str__", "rating_average")
